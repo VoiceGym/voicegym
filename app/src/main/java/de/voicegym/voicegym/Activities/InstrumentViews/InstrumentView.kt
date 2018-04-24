c65ccaffff5721e88cdf7ffb8f2161c6e591c85d
KayleeTheMech <kaylee@posteo.de>
2018-04-24 11:50:36 +0200

    Made InstrumentView abstract, and added some required functionality

--- app/src/main/java/de/voicegym/voicegym/Activities/InstrumentViews/InstrumentView.kt
+++ app/src/main/java/de/voicegym/voicegym/Activities/InstrumentViews/InstrumentView.kt
@@ -1,12 +1,10 @@
 package de.voicegym.voicegym.Activities.InstrumentViews
 
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Rect
 import android.util.AttributeSet
 import android.view.View
+import de.voicegym.voicegym.FourierHelper.RequestedResult
+import de.voicegym.voicegym.FourierHelper.RequestedResultType
 
 abstract class InstrumentView : View {
 
@@ -18,5 +16,26 @@ abstract class InstrumentView : View {
 
     }
 
+    /**
+     * The {@link RequestedResultType} RequestedResultType this implementation of the InstrumentView needs.
+     *
+     * This has to be overridden in concrete implementation of this class.
+     */
+    abstract val requiredResultType: RequestedResultType
 
+    protected abstract fun addCheckedResult(requestedResult: RequestedResult)
+
+    /**
+     * @param requestedResult: The result to display in this InstrumentView
+     */
+    fun addResult(requestedResult: RequestedResult) {
+        if (requestedResult.resultType == requiredResultType) addCheckedResult(requestedResult)
+    }
+
+    /**
+     * This needs to be called once to tell the view the scale of the abscissa.
+     *
+     * @param resultDuration: the measurement time taken to capture the result (determined by binning and the blocklength of the fouriertransform)
+     */
+    abstract fun setResultDuration(resultDuration: Double)
 }
