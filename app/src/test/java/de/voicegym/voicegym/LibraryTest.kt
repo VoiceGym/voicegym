0500459f474d22cbec47febca99ea38cae5983da
Saskia Gennrich <saskia.gennrich@posteo.de>
2018-05-25 17:21:30 +0200

    move wav file test to own file

--- app/src/test/java/de/voicegym/voicegym/LibraryTest.kt
+++ app/src/test/java/de/voicegym/voicegym/LibraryTest.kt
@@ -3,8 +3,7 @@ package de.voicegym.voicegym
 //import com.google.common.base.Stopwatch
 
 import de.voicegym.voicegym.audioHelper.WavFile
-import de.voicegym.voicegym.fourierHelper.PCMUtil.getDoubleArrayFromShortArray
-import junit.framework.Assert.assertEquals
+import de.voicegym.voicegym.fourierHelper.getDoubleArrayFromShortArray
 import org.jtransforms.fft.DoubleFFT_1D
 import org.junit.Test
 import java.io.File
@@ -25,18 +24,7 @@ class LibraryTest {
     }
 
 
-    @Test
-    fun testLoadWavFile() {
-        val file: File = File("src/test/resources/frame1.wav")
 
-        // 1 ms of data
-        val expected = arrayOf(10465, 10210, 8824, 8648, 7875, 6984, 7424, 7095, 6985, 7293, 6472, 5845, 5337, 4336, 3898, 3571, 2953, 2143, 1343, 164, -1122, -1646, -2091, -2374, -1920, -2124, -2494, -2715, -3811, -4253, -4284, -4270, -3350, -2124, -887, 679, 1997, 3171, 4476, 5262, 5893, 6686, 6882, 7169)
-
-        WavFile(file).getTimeFrame(1).forEachIndexed { i, pcm ->
-            assertEquals(expected.get(i).toShort(), pcm)
-        }
-
-    }
 
     @Test
     fun testFireJtransformFourier() {
