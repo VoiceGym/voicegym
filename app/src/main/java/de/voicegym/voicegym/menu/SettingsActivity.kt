af452f66a3951978c20cd6df0b3f15b5ca722723
KayleeTheMech <kaylee@posteo.de>
2018-06-15 12:00:57 +0200

    Settings general umbenannt, und alte general settings gelöscht

--- app/src/main/java/de/voicegym/voicegym/menu/SettingsActivity.kt
+++ app/src/main/java/de/voicegym/voicegym/menu/SettingsActivity.kt
@@ -4,8 +4,6 @@ import android.annotation.TargetApi
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
-import android.media.RingtoneManager
-import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.preference.CheckBoxPreference
@@ -15,9 +13,6 @@ import android.preference.Preference
 import android.preference.PreferenceActivity
 import android.preference.PreferenceFragment
 import android.preference.PreferenceManager
-import android.preference.RingtonePreference
-import android.text.TextUtils
-import android.util.Log
 import android.view.MenuItem
 import de.voicegym.voicegym.R
 
@@ -190,28 +185,6 @@ class SettingsActivity : AppCompatPreferenceActivity() {
                         else
                             null)
 
-            } else if (preference is RingtonePreference) {
-                // For ringtone preferences, look up the correct display value
-                // using RingtoneManager.
-                if (TextUtils.isEmpty(stringValue)) {
-                    // Empty values correspond to 'silent' (no ringtone).
-                    preference.setSummary(R.string.pref_ringtone_silent)
-
-                } else {
-                    val ringtone = RingtoneManager.getRingtone(
-                            preference.getContext(), Uri.parse(stringValue))
-
-                    if (ringtone == null) {
-                        // Clear the summary if there was a lookup error.
-                        preference.setSummary(null)
-                    } else {
-                        // Set the summary to reflect the new ringtone display
-                        // name.
-                        val name = ringtone.getTitle(preference.getContext())
-                        preference.setSummary(name)
-                    }
-                }
-
             } else {
                 // For all other preferences, set the summary to the value's
                 // simple string representation.
@@ -244,18 +217,16 @@ class SettingsActivity : AppCompatPreferenceActivity() {
             // Trigger the listener immediately with the preference's
             // current value.
 
-            //Log.i("SettingsActivity", "preference of class" + preference.javaClass.toString())
+            val sharedPreferences = PreferenceManager
+                    .getDefaultSharedPreferences(preference.context)
+
             val value = when (preference) {
                 is CheckBoxPreference -> {
-                    PreferenceManager
-                            .getDefaultSharedPreferences(preference.context)
-                            .getBoolean(preference.key, false)
+                    sharedPreferences.getBoolean(preference.key, false)
                 }
 
                 is EditTextPreference -> {
-                    PreferenceManager
-                            .getDefaultSharedPreferences(preference.context)
-                            .getString(preference.key, "")
+                    sharedPreferences.getString(preference.key, "")
                 }
 
                 else                  -> throw Error("Introduced new type of preference, needs to be caught here")
