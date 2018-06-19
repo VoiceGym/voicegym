e9868dd8c5b26579516f215ac0ca6f6302f26a57
KayleeTheMech <kaylee@posteo.de>
2018-06-19 16:10:54 +0200

    commented the InstrumentFragmentInterface

--- app/src/main/java/de/voicegym/voicegym/recordActivity/fragments/InstrumentFragmentInterface.kt
+++ app/src/main/java/de/voicegym/voicegym/recordActivity/fragments/InstrumentFragmentInterface.kt
@@ -2,23 +2,56 @@ package de.voicegym.voicegym.recordActivity.fragments
 
 import de.voicegym.voicegym.menu.settings.FourierInstrumentViewSettings
 
+/**
+ * this interface has to be implemented in order to be able to use a fragment as an instrument within the RecordActivity
+ */
 interface InstrumentFragmentInterface {
+
+    /**
+     * passes along the current settings
+     */
     fun updateInstrumentViewSettings(settings: FourierInstrumentViewSettings)
 
+    /**
+     * passes the fourierhelpers frequency array
+     */
     fun updateFrequencyArray(frequencies: DoubleArray)
 
+    /**
+     * passes along an array of intensities in the frequency space
+     */
     fun insertNewAmplitudes(spectrum: DoubleArray)
 
+    /**
+     * settings have to publicly accessible
+     */
     var settings: FourierInstrumentViewSettings
 
+    /**
+     * return the currently displayed sample number
+     */
     fun getCurrentSamplePosition(): Int
 
+    /**
+     * scroll the instrument
+     *
+     * @param samplePosition: scroll to this sample number
+     */
     fun seekToSamplePosition(samplePosition: Int)
 
+    /**
+     * reset the fragment and start over in Live_display mode
+     */
     fun resetFragment()
 
+    /**
+     * start into recording mode
+     */
     fun startRecording()
 
+    /**
+     * switch into playback mode
+     */
     fun doneRecordingSwitchToPlayback()
 
     fun getInstrumentState(): InstrumentState
